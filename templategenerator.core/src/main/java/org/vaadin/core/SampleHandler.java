f92306b99694ae299b3a41b667f5e0dded3a3cec
Matti Hosio <mhosio@vaadin.com>
2015-09-23 15:43:06 +0300

    Menu selection working now

--- templategenerator.core/src/main/java/org/vaadin/core/SampleHandler.java
+++ templategenerator.core/src/main/java/org/vaadin/core/SampleHandler.java
@@ -3,10 +3,15 @@ package org.vaadin.core;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.ISources;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
  * Executed by click menu.<br/>
@@ -28,7 +33,17 @@ public class SampleHandler extends AbstractHandler {
      */
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
-        MessageDialog.openInformation(window.getShell(), "Eclipse Plugin Archetype", "Hello, Maven+Eclipse world,\n templategenerator is built with Tycho");
+    	Object variable = HandlerUtil.getVariable(event, ISources.ACTIVE_CURRENT_SELECTION_NAME);
+    	IStructuredSelection selection = (IStructuredSelection)  variable;
+    	Object firstElement = selection.getFirstElement();
+    	ICompilationUnit file = (ICompilationUnit) firstElement;
+    	try {
+        	String sourceCode=file.getSource();
+        	
+		} catch (JavaModelException e) {
+			MessageDialog.openInformation(window.getShell(), "Error while exporting design", e.getCause().toString());
+			e.printStackTrace();
+		}
         return null;
     }
 
